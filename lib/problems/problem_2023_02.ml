fb8cafc3b8fcad9f5183f32a84072b21b1fe75c4
George Shammas <george@shamm.as>
2023-12-03 06:08:05 -0500

    2023-02 part2

--- lib/problems/problem_2023_02.ml
+++ lib/problems/problem_2023_02.ml
@@ -17,11 +17,41 @@ module Cubes = struct
     | _ -> failwith "impossible color"
   ;;
 
+  let value t =
+    match t with
+    | Red v | Blue v | Green v -> v
+  ;;
+
+  let of_game input =
+    let round_spit round =
+      String.split ~on:',' round
+      |> List.map ~f:(fun r' ->
+        String.strip r'
+        |> String.split ~on:' '
+        |> function
+        | [ value; color ] -> of_split color (Int.of_string value)
+        | _ as failure ->
+          failwith
+            ([%message "could not parse" (round : string) (failure : string List.t)]
+             |> Sexp.to_string))
+    in
+    String.split ~on:';' input |> List.map ~f:round_spit
+  ;;
+
   let over_max t =
     match t with
     | Red v -> v > 12
     | Blue v -> v > 14
     | Green v -> v > 13
+  ;;
+
+  let max tlist =
+    List.fold tlist ~init:(Red 1, Blue 1, Green 1) ~f:(fun (red, blue, green) t ->
+      (match t with
+       | Red v as red' -> Option.some_if (v > value red) (red', blue, green)
+       | Blue v as blue' -> Option.some_if (v > value blue) (red, blue', green)
+       | Green v as green' -> Option.some_if (v > value green) (red, blue, green'))
+      |> Option.value ~default:(red, blue, green))
   ;;
 end
 
@@ -31,25 +61,14 @@ module Part_1 = struct
       String.split_lines input
       |> List.map ~f:(fun line -> String.split line ~on:':' |> List.last_exn)
     in
-    let round_is_over round =
-      String.split ~on:',' round
-      |> List.map ~f:(fun r' ->
-        String.strip r'
-        |> String.split ~on:' '
-        |> function
-        | [ value; color ] -> Cubes.of_split color (Int.of_string value)
-        | _ as failure ->
-          failwith
-            ([%message "could not parse" (round : string) (failure : string List.t)]
-             |> Sexp.to_string))
-      |> List.fold ~init:false ~f:(fun acc cube -> acc || Cubes.over_max cube)
+    let games =
+      List.map input ~f:(fun game ->
+        Cubes.of_game game
+        |> List.fold ~init:false ~f:(fun acc round ->
+          acc
+          || List.fold round ~init:false ~f:(fun acc color -> acc || Cubes.over_max color)))
     in
-    let game_is_over game =
-      String.split ~on:';' game
-      |> List.fold ~init:false ~f:(fun acc round -> acc || round_is_over round)
-    in
-    let input = List.map input ~f:game_is_over in
-    List.foldi input ~init:0 ~f:(fun idx0 acc game_result ->
+    List.foldi games ~init:0 ~f:(fun idx0 acc game_result ->
       if game_result then acc else acc + idx0 + 1)
     |> Int.to_string
     |> Result.return
@@ -57,5 +76,17 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let run input : (string, string) result = Ok input
+  let run input : (string, string) result =
+    let input =
+      String.split_lines input
+      |> List.map ~f:(fun line -> String.split line ~on:':' |> List.last_exn)
+    in
+    let game_maxs =
+      List.map input ~f:(fun game -> Cubes.of_game game |> List.concat |> Cubes.max)
+    in
+    List.fold game_maxs ~init:0 ~f:(fun acc (red, blue, green) ->
+      acc + Cubes.(value red * value blue * value green))
+    |> Int.to_string
+    |> Result.return
+  ;;
 end
