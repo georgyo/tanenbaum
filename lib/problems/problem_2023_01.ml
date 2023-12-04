1477cd6011839cd1e60aed4065e2de9c9d707a8f
George Shammas <george@shamm.as>
2023-12-03 20:33:11 -0500

    2023-01 part2

--- lib/problems/problem_2023_01.ml
+++ lib/problems/problem_2023_01.ml
@@ -25,5 +25,36 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let run input : string Or_error.t = Ok input
+  let rec firstlastdigit (first, last) clist =
+    let helper n tl = firstlastdigit (Some (Option.value ~default:n first), Some n) tl in
+    match clist with
+    | [] -> first, last
+    | ('1' .. '9' as n) :: tl -> helper n tl
+    | 'o' :: ('n' :: 'e' :: _ as tl) -> helper '1' tl
+    | 't' :: ('w' :: 'o' :: _ as tl) -> helper '2' tl
+    | 't' :: ('h' :: 'r' :: 'e' :: 'e' :: _ as tl) -> helper '3' tl
+    | 'f' :: ('o' :: 'u' :: 'r' :: _ as tl) -> helper '4' tl
+    | 'f' :: ('i' :: 'v' :: 'e' :: _ as tl) -> helper '5' tl
+    | 's' :: ('i' :: 'x' :: _ as tl) -> helper '6' tl
+    | 's' :: ('e' :: 'v' :: 'e' :: 'n' :: _ as tl) -> helper '7' tl
+    | 'e' :: ('i' :: 'g' :: 'h' :: 't' :: _ as tl) -> helper '8' tl
+    | 'n' :: ('i' :: 'n' :: 'e' :: _ as tl) -> helper '9' tl
+    | _ :: tl -> firstlastdigit (first, last) tl
+  ;;
+
+  let firstlastdigit clist =
+    firstlastdigit (None, None) clist
+    |> function
+    | Some f, Some l -> Some (String.of_char_list [ f; l ] |> Int.of_string)
+    | _ -> None
+  ;;
+
+  let run input : string Or_error.t =
+    String.split_lines input
+    |> List.map ~f:String.to_list
+    |> List.filter_map ~f:firstlastdigit
+    |> List.fold ~init:0 ~f:( + )
+    |> Int.to_string
+    |> return
+  ;;
 end
