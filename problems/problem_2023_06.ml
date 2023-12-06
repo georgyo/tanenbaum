6ef612a304301d5fea57679aaa8ba37701db4568
George Shammas <george@shamm.as>
2023-12-06 04:57:09 -0500

    2023-06 part2

--- problems/problem_2023_06.ml
+++ problems/problem_2023_06.ml
@@ -53,7 +53,39 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let run input : string Or_error.t = Ok input
+  let run input : string Or_error.t =
+    let input =
+      String.split_lines input
+      |> List.map ~f:(fun line ->
+        String.split line ~on:':'
+        |> List.last_exn
+        |> String.filter ~f:Char.is_digit
+        |> Int.of_string)
+    in
+    let%map time, distance =
+      match input with
+      | [ time; distance ] -> (time, distance) |> return
+      | _ -> Or_error.error_s [%message "unknown input" (input : int list)]
+    in
+    let can_win ~speed time distance = speed * (time - speed) > distance in
+    let rec min_max ~time ~distance ~speed (min, max) =
+      if speed >= time
+      then min, max
+      else
+        min_max
+          ~time
+          ~distance
+          ~speed:(speed + 1)
+          (if can_win ~speed time distance
+           then Option.value ~default:speed min |> Option.return, Option.return speed
+           else min, max)
+    in
+    (match min_max ~time ~distance ~speed:1 (None, None) with
+     | Some min, Some max -> max - min + 1
+     | _, _ ->
+       raise_s [%message "could not find min max" (time : Int.t) (distance : Int.t)])
+    |> Int.to_string
+  ;;
 
   let%expect_test _ =
     run test_data |> Or_error.ok_exn |> print_endline;
